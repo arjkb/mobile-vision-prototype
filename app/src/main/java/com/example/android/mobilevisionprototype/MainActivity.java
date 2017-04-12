ecb042322a1b68461f6f0071ba15b88169e7c088
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-11 22:48:48 -0600

    adding an OnClickListener for the button

--- app/src/main/java/com/example/android/mobilevisionprototype/MainActivity.java
+++ app/src/main/java/com/example/android/mobilevisionprototype/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.android.mobilevisionprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView mImageView;
     Button mProcessButton;
@@ -17,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
         mImageView = (ImageView) findViewById(R.id.image);
         mProcessButton = (Button) findViewById(R.id.button);
+
+        mProcessButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())  {
+            case R.id.button:
+                Toast.makeText(this, "Process button clicked!", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
