79e5b480aecd7c7662605ce5895e23c496f18e37
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-11 23:19:43 -0600

    adding function to detect text

--- app/src/main/java/com/example/android/mobilevisionprototype/MainActivity.java
+++ app/src/main/java/com/example/android/mobilevisionprototype/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.android.mobilevisionprototype;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.vision.text.TextRecognizer;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView mImageView;
     Button mProcessButton;
+
+    final String TAG = "MV";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,4 +39,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
+    public void detectText() {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if(!textRecognizer.isOperational()) {
+            Log.w(TAG, "Detector dependencies are not yet available!");
+
+            IntentFilter lowStorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowStorageFilter) != null;
+
+            if (hasLowStorage)  {
+                Toast.makeText(this, "Low storage! ", Toast.LENGTH_SHORT).show();
+                Log.w(TAG, "Low storage!");
+            }
+        }
+    }
 }
+
