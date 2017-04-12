e113dfdbb45af5196f640ab930790ae6eb0a69e8
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-11 22:37:34 -0600

    adding variable handles to button and image-view

--- app/src/main/java/com/example/android/mobilevisionprototype/MainActivity.java
+++ app/src/main/java/com/example/android/mobilevisionprototype/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.android.mobilevisionprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView mImageView;
+    Button mProcessButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+        mProcessButton = (Button) findViewById(R.id.button);
     }
 }
