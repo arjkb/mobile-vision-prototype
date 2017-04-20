d843c0e95fa2b94d1c20a47ba4d7dcb89d7d7026
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-20 17:56:49 -0600

    creating a basic Dialog

--- app/src/main/java/com/example/android/mobilevisionprototype/CreateEventDialogFragment.java
+++ app/src/main/java/com/example/android/mobilevisionprototype/CreateEventDialogFragment.java
@@ -1,16 +1,41 @@
 package com.example.android.mobilevisionprototype;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by akbfedora on 4/20/17.
  */
 
 public class CreateEventDialogFragment extends DialogFragment {
+
+    final String TAG = "CEDF";
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setMessage("Blah!")
+                .setTitle("Blah Title")
+                .setPositiveButton("OKAY", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(getContext(), "Positive press!", Toast.LENGTH_SHORT);
+                        Log.v(TAG, "Positive press!");
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(getContext(), "Negative press!", Toast.LENGTH_SHORT);
+                        Log.v(TAG, "Negative press!");
+                    }
+                });
+
+        return builder.create();
     }
 }
