f09419e68cddaad034882ca915d31c85cb5ac49b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-20 17:49:23 -0600

    creating new file for displaying Dialog

---
+++ app/src/main/java/com/example/android/mobilevisionprototype/CreateEventDialogFragment.java
@@ -0,0 +1,16 @@
+package com.example.android.mobilevisionprototype;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by akbfedora on 4/20/17.
+ */
+
+public class CreateEventDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
