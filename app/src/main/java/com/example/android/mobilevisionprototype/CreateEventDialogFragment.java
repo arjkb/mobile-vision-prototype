578e6b5aae637cb686e4bb4f759493075ab31f23
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-20 18:08:54 -0600

    changing DialogFragment to the version in V4 support library

--- app/src/main/java/com/example/android/mobilevisionprototype/CreateEventDialogFragment.java
+++ app/src/main/java/com/example/android/mobilevisionprototype/CreateEventDialogFragment.java
@@ -1,8 +1,9 @@
 package com.example.android.mobilevisionprototype;
 
+
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
